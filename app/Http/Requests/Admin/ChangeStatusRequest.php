c8b059ed55244beee2b0c4a94f8f57e1e4d7f903
mestashh <zeuszeus1973@gmail.com>
2026-03-11 21:40:53 +0300

    status change

---
+++ app/Http/Requests/Admin/ChangeStatusRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use App\Enums\StatusEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class ChangeStatusRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'access_status' => ['required', new Enum(StatusEnum::class)],
+        ];
+    }
+}
