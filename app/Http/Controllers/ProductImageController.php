ad68e50b9d53171429b8e0d9175a1fbac05d284f
mestashh <zeuszeus1973@gmail.com>
2026-03-16 17:05:19 +0300

    ProductVariant API

--- app/Http/Controllers/ProductImageController.php
+++ app/Http/Controllers/ProductImageController.php
@@ -2,64 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductImage\StoreProductImageRequest;
+use App\Http\Requests\ProductImage\UpdateProductImageRequest;
+use App\Http\Resources\ProductImageResource;
+use App\Models\Product;
 use App\Models\ProductImage;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class ProductImageController
+class ProductImageController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Product $product)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return ProductImageResource::collection($product->productImages()->paginate(20));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductImageRequest $request, Product $product)
     {
-        //
+        $this->authorize('create', [ProductImage::class, $product]);
+        $data = $request->validated();
+        $data['position'] = $product->productImages()->max('position') + 1;
+        $image = $product->productImages()->create($data);
+
+        return new ProductImageResource($image);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(ProductImage $productImages)
+    public function show(ProductImage $productImage)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(ProductImage $productImages)
-    {
-        //
+        return new ProductImageResource($productImage);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, ProductImage $productImages)
+    public function update(UpdateProductImageRequest $request, Product $product, ProductImage $productImage)
     {
-        //
+        $this->authorize('update', [ProductImage::class, $productImage]);
+        $productImage->update($request->validated());
+
+        return new ProductImageResource($productImage);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ProductImage $productImages)
+    public function destroy(Product $product, ProductImage $productImage)
     {
-        //
+        $this->authorize('delete', [ProductImage::class, $productImage]);
+        Storage::disk('public')->delete($productImage->path);
+        $productImage->delete();
+
+        return response()->noContent();
     }
 }
